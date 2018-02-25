ae851f31bacb6f94f06a686935014a463defd7bc
rahulbittu <36802634+rahulbittu@users.noreply.github.com>
2018-02-24 21:25:01 -0600

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.get('/article-one',function(req,res){
 });
 
 app.get('/article-three',function(req,res){
-    res.send('Article one requested and will be served here');
+    res.send('Article three requested and will be served here');
 });
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
