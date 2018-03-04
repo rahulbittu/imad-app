87551f0a211e7417d728d9236a9fa5058376a2fc
rahulbittu <36802634+rahulbittu@users.noreply.github.com>
2018-03-03 21:48:09 -0600

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,19 +1,8 @@
-console.log('Loaded!');
-
-var element = document.getElementById("main-text");
-
-element.innerHTML="New Value";
-
-var img = document.getElementById("madi");
-
-var marginLeft=0;
-
-function moveRight(){
-    marginLeft= marginLeft+10;
-    img.style.marginLeft=marginLeft+"px";
-}
-img.onclick = function(){
+var button = document.getElementById("counter");
+var counter =0;
+button.onClick = function(){
     
-    var interval = setInterval(moveRight,100);
-    
+    counter = counter+1;
+    var span = document.getElementById("count");
+    span.innerHTML = counter.toString();
 }
