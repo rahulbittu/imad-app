097b6b5a8488fe96eee34640d5483d13529d90d4
Rahul Pitta <36802634+rahulbittu@users.noreply.github.com>
2018-03-31 14:39:59 -0500

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,22 +8,13 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+
 var counter=0;
 app.get('/counter', function (req, res) {
   counter=counter+1;
   res.sendFile(counter.toString());
 });
-app.get('/article-two',function(req,res){
-    res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
-});
 
-app.get('/article-one',function(req,res){
-    res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
-});
-
-app.get('/article-three',function(req,res){
-    res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
-});
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
